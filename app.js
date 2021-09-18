7514d3e25cb617e06a7bbbce86ea7eaece476a4e
WAYNE CHOI <wonjun92@gmail.com>
2021-09-18 23:56:07 +1200

    fix deploy issue

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
 import http from "http";
-
 import screenshotRouter from "./router/screenshot.js";
 
 const app = express();
 app.use(cors());
-
-setInterval(function () {
-  http.get("https://apiwayne.herokuapp.com/");
-}, 600000);
-
+app.use(() => {
+  setInterval(function () {
+    http.get("https://apiwayne.herokuapp.com/");
+  }, 600000);
+});
 app.use("/screenshot", screenshotRouter);
 
 app.use((req, res, next) => {
